2ad9a710c34ff45c6123cbe0d92bd948f0412ee5
StpME <54916725+StpME@users.noreply.github.com>
2020-05-31 00:00:25 -0700

    setup random num method and rect color delay

--- Animation/src/myMasterpiece/Main.java
+++ Animation/src/myMasterpiece/Main.java
@@ -13,9 +13,10 @@ public class Main extends PApplet
 {
 	// Declare any private fields you need to help keep track of
 	// stuff while your masterpiece animates itself
-
+	private int delay;
 	public static void main(String[] args) 
 	{
+		
 		// This string MUST match your package and class name
 		PApplet.main("myMasterpiece.Main");
 
@@ -43,11 +44,29 @@ public class Main extends PApplet
 	// Do any one-time initialization here, like initializing fields
 	public void setup()
 	{
+		this.delay = 0;
 	}
-
+	
+	//gives random num from 0-255
+	public int randomNum()
+	{
+		int num = 0;
+		num  = (int)(Math.random() * 256);
+		//System.out.println(num);
+		return num;
+	}
+	
+	
 	// This gets called over and over again, once for each animation frame
 	public void draw() 
 	{
+		int num = randomNum();
+		
+		
+		if(num == 1)
+		{ 
+			
+		}
 		// Typically, you'll do something like this to clear the
 		// screen before drawing your frame.  Feel free to change
 		// the color.
@@ -56,6 +75,23 @@ public class Main extends PApplet
 		// Then call methods on g to draw stuff.  This is just an example,
 		// feel free to remove.  See the links at the top of this file
 		// for documentation on the drawing methods you can call on g
-		g.line(0, 0, g.width, g.height);
+		
+		
+		
+		
+		
+		//sets stroke speed 
+		if(delay == 10)
+		{
+			g.stroke(randomNum(),randomNum(),randomNum());
+			delay = 0;
+			System.out.println("w");
+		}
+		else
+		{
+			delay++;
+		}
+		//g.stroke(0,0,0);
+		g.rect(150, 150, 324, 124);
 	}
 }
